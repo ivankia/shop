be0c0b35208453605ff5477881697aa49aa36e8a
Ivan Kuznetsov <ivan_kuznetsov@bk.ru>
2016-12-15 19:23:08 +0700

    first commit

---
+++ protected/config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	'connectionString' => 'mysql:host=localhost;dbname=shop',
+	'emulatePrepare' => true,
+	'username' => 'root',
+	'password' => 'root',
+	'charset' => 'utf8',
+);
---
+++ protected/migrations/m161215_094943_initial.php
@@ -0,0 +1,60 @@
+<?php
+
+class m161215_094943_initial extends CDbMigration
+{
+	public function up()
+	{
+        $this->createTable('users', [
+            'user_id' => 'pk',
+            'name' => 'string not null',
+            'email' => 'string not null',
+            'password' => 'string not null',
+            'active' => 'boolean not null default 1',
+        ]);
+
+        $this->createTable('products', [
+            'product_id' => 'pk',
+            'name' => 'string not null',
+            'description' => 'text not null',
+            'price' => 'money',
+            'currency_id' => 'integer not null',
+            'created_at' => 'datetime default CURRENT_TIMESTAMP',
+            'modified_at' => 'datetime default CURRENT_TIMESTAMP',
+            'deleted_date' => 'datetime',
+        ]);
+
+        $this->createTable('orders', [
+            'order_id' => 'pk',
+            'email' => 'string not null',
+            'product_id' => 'integer not null',
+            'price' => 'money',
+            'currency_id' => 'integer not null',
+            'created_at' => 'datetime default CURRENT_TIMESTAMP',
+            'modified_at' => 'datetime default CURRENT_TIMESTAMP',
+            'status' => 'integer not null default 0',
+        ]);
+
+        $this->createTable('currency', [
+            'currency_id' => 'pk',
+            'code' => 'string not null',
+            'ratio' => 'float',
+            'deleted_date' => 'datetime',
+        ]);
+
+        $this->addForeignKey('products_currency_fk_1', 'products', 'currency_id', 'currency', 'currency_id', 'RESTRICT', 'CASCADE');
+        $this->addForeignKey('orders_products_fk_1', 'orders', 'product_id', 'products', 'product_id', 'RESTRICT', 'CASCADE');
+        $this->addForeignKey('orders_currency_fk_1', 'orders', 'currency_id', 'currency', 'currency_id', 'RESTRICT', 'CASCADE');
+
+        return true;
+	}
+
+	public function down()
+	{
+		$this->dropTable('users');
+		$this->dropTable('products');
+		$this->dropTable('orders');
+		$this->dropTable('currency');
+
+		return true;
+	}
+}
